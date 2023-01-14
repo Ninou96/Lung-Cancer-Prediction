4d4d673dad6a4b8b5da765b7567eb150370166cf
Nidhal Saoudi <107215037+Ninou96@users.noreply.github.com>
2023-01-14 15:20:16 +0100

    Add files via upload

---
+++ my_project.py
@@ -0,0 +1,52 @@
+import streamlit
+from joblib import dump, load
+import torch
+import joblib
+from sklearn.linear_model import LogisticRegression
+
+
+
+# Save the trained model to a file
+joblib.dump(model, 'trained_model.pkl')
+
+# Load the trained model from a file
+model = joblib.load('trained_model.pkl')
+
+# Use the model to make predictions
+predictions = model.predict(X_test)
+import streamlit as st
+import joblib
+
+# Load the trained model
+model = joblib.load('trained_model.pkl')
+
+def predict_lung_cancer(age, gender, smoking, alcohol_use, occupational_hazards, chest_pain, snoring, chronic_lung_disease, obesity, coughing_of_blood, shortness_of_breath, wheezing, frequent_cold):
+    """Takes user input and returns a prediction of lung cancer risk."""
+    # Prepare the input data as a numpy array
+    input_data = [age, gender, smoking, alcohol_use, occupational_hazards, chest_pain, snoring, chronic_lung_disease, obesity, coughing_of_blood, shortness_of_breath, wheezing, frequent_cold]
+    input_data = np.array(input_data).reshape(1, -1)
+    # Make a prediction using the trained model
+    prediction = model.predict(input_data)
+    return prediction
+
+def main():
+    st.title("Lung Cancer Risk Prediction")
+    age = st.number_input("Age")
+    gender = st.selectbox("Gender", ["Male", "Female"])
+    smoking_status = st.selectbox("Smoking Status", ["Never smoked", "Former smoker", "Current smoker"])
+    alcohol_use = st.selectbox("Alcohol use", ["Never", "Occasionally", "Regularly"])
+    occupational_hazards = st.selectbox("Occupational hazards", ["No", "Yes"])
+    chest_pain = st.selectbox("Chest pain", ["No", "Yes"])
+    snoring = st.selectbox("Snoring", ["No", "Yes"])
+    chronic_lung_disease = st.selectbox("Chronic lung disease", ["No", "Yes"])
+    obesity = st.selectbox("Obesity", ["No", "Yes"])
+    coughing_of_blood = st.selectbox("Coughing of blood", ["No", "Yes"])
+    shortness_of_breath = st.selectbox("Shortness of breath", ["No", "Yes"])
+    wheezing = st.selectbox("Wheezing", ["No", "Yes"])
+    frequent_cold = st.selectbox("Frequent cold", ["No", "Yes"])
+    
+    # Other input fields for other features
+
+    if st.button("Predict"):
+        result = predict_lung_cancer(age, gender, smoking_status, alcohol_use, occupational_hazards, chest_pain, snoring, chronic_lung_disease, obesity, coughing_of_blood, shortness_of_breath, wheezing, frequent_cold, ...)
+        st.success("The predicted risk of lung cancer is: {}".format(result))
